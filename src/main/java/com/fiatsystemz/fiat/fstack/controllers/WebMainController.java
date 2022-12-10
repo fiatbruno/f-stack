a2bcc88edd214395945460a9902141e5aa93d887
Fiat Bruno <fiat@Fiats-MacBook-Air.local>
2022-12-10 04:32:28 +0200

    f-stack

---
+++ src/main/java/com/fiatsystemz/fiat/fstack/controllers/WebMainController.java
@@ -0,0 +1,14 @@
+package com.fiatsystemz.fiat.fstack.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebMainController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+}
+
---
+++ src/test/java/com/fiatsystemz/fiat/fstack/SpringBootReactJsFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiatsystemz.fiat.fstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactJsFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
